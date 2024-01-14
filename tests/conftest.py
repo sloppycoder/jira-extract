def086d516a773cb815bc64f3b53f22756c9c274
Li Lin <guru.lin@gmail.com>
2024-01-14 14:15:32 +0800

    generated by cookiecutter

---
+++ tests/conftest.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+
+import pytest
+from alembic import command
+from alembic.config import Config
+from loguru import logger
+from sqlalchemy import create_engine
+from sqlalchemy.engine import make_url
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists, drop_database
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(f"{cwd}/.."))
+
+
+from jira_extract.models import User  # noqa: E402
+
+
+@pytest.fixture(scope="session")
+def sql_engine(tmp_path_factory):
+    test_db_url = os.environ.get("TEST_DATABASE_URL")
+    if test_db_url is None:
+        test_db_url = f"sqlite:///{tmp_path_factory.getbasetemp()}/test.db"
+
+    test_db_url = make_url(url_str)
+    if test_db_url.drivername.startswith("postgres"):
+        if not test_db_url.username:
+            test_db_url = test_db_url.set(username=os.environ.get("TEST_PGUSER"))
+        if not test_db_url.password:
+            test_db_url = test_db_url.set(password=os.environ.get("TEST_PGPASSWORD"))
+
+    if not database_exists(test_db_url):
+        logger.info(f"creating test database {test_db_url}")
+        create_database(test_db_url)
+
+    # Run the migrations
+    # migrations/env.py sets the sqlalchemy.url using env var DATABASE_URL
+    # so we set it so that alembic knows to use the correct database during testing
+    os.environ["DATABASE_URL"] = test_db_url.render_as_string(hide_password=False)
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+    sql_engine = create_engine(test_db_url)
+
+    # seed test data
+    with sessionmaker(bind=sql_engine)() as session:
+        seed_data(session)
+
+    yield sql_engine
+
+    if os.environ.get("KEEP_TEST_DB").upper() not in ["1", "Y", "YES", "TRUE"]:
+        # drop the test database
+        logger.info(f"dropping test database {test_db_url}")
+        drop_database(test_db_url)
+
+
+@pytest.fixture(scope="session")
+def session(sql_engine):
+    Session = sessionmaker(bind=sql_engine)
+    session = Session()
+
+    yield session
+
+    session.close()
+
+
+def seed_data(session):
+    root = User(login_name="root")
+    session.add(root)
+    session.commit()
+
---
+++ tests/test_models.py
@@ -0,0 +1,6 @@
+from jira_extract.models import User
+
+
+def test_query_models(session):
+    user = session.query(User).filter_by(login_name="root").first()
+    assert user.id == 1
